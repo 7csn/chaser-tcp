70d09750946b2063d8c1460dd10d37b546f0e5d8
7csn <1479672753@qq.com>
2021-06-06 12:25:57 +0800

    .

--- src/Connection.php
+++ src/Connection.php
@@ -12,44 +12,8 @@ use chaser\stream\traits\NetworkAddress;
  * tcp 连接
  *
  * @package chaser\tcp
- *
- * @property-read Server $server
- * @property-read int $heartbeatTime
  */
 class Connection extends StreamConnection implements NetworkAddressInterface
 {
     use NetworkAddress;
-
-    /**
-     * 心跳时间
-     *
-     * @var int
-     */
-    protected int $heartbeatTime = 0;
-
-    /**
-     * @inheritDoc
-     */
-    protected function initCommon(): void
-    {
-        $this->heartbeat();
-        parent::initCommon();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    protected function dispatchMessage(mixed $message): void
-    {
-        $this->heartbeat();
-        parent::dispatchMessage($message);
-    }
-
-    /**
-     * 心跳
-     */
-    private function heartbeat(): void
-    {
-        $this->heartbeatTime = time();
-    }
 }
--- src/Server.php
+++ src/Server.php
@@ -11,44 +11,10 @@ use chaser\stream\traits\ServerContext;
  * tcp 服务器
  *
  * @package chaser\tcp
- *
- * @property Connection[] $connections
- *
- * @property-read int $heartbeatTimeout
- * @property-read int $checkHeartbeatInterval
  */
 class Server extends ConnectedServer
 {
     use ServerContext, Service;
-
-    /**
-     * 默认心跳（收到新的完整请求）时间间隔上限（秒）
-     */
-    public const HEARTBEAT_TIMEOUT = 55;
-
-    /**
-     * 默认心跳（收到新的完整请求）检测时间间隔（秒）
-     */
-    public const CHECK_HEARTBEAT_INTERVAL = 10;
-
-    /**
-     * 心电图 ID
-     *
-     * @var int
-     */
-    private int $ekgId = 0;
-
-    /**
-     * @inheritDoc
-     */
-    public static function configurations(): array
-    {
-        return [
-                'heartbeatTimeout' => self::HEARTBEAT_TIMEOUT,
-                'checkHeartbeatInterval' => self::CHECK_HEARTBEAT_INTERVAL
-            ]
-            + parent::configurations();
-    }
 
     /**
      * @inheritDoc
@@ -71,52 +37,9 @@ class Server extends ConnectedServer
     /**
      * @inheritDoc
      */
-    protected function running(): void
-    {
-        $this->onEkg();
-        parent::running();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    protected function close(): void
-    {
-        $this->offEkg();
-        parent::close();
-    }
-
-    /**
-     * @inheritDoc
-     */
     protected function configureSocket(): void
     {
         $this->socketSettings();
         parent::configureSocket();
-    }
-
-    /**
-     * 打开心电图
-     */
-    private function onEkg(): void
-    {
-        $this->ekgId = $this->reactor->setInterval($this->checkHeartbeatInterval, function () {
-            $now = time();
-            foreach ($this->connections as $connection) {
-                if ($connection->heartbeatTime + $this->heartbeatTimeout < $now) {
-                    $connection->close();
-                }
-            }
-        });
-    }
-
-    /**
-     * 关闭心电图
-     */
-    private function offEkg(): void
-    {
-        if ($this->ekgId > 0) {
-            $this->reactor->delInterval($this->ekgId);
-        }
     }
 }
